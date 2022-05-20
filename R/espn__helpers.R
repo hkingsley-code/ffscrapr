f8f667f949ce1a37e9e4d420379c4e3b2d57392b
hkingsley-code <harriskingsley3419@gmail.com>
2022-05-20 13:00:01 -0700

    update points to baseball

--- R/espn__helpers.R
+++ R/espn__helpers.R
@@ -197,91 +197,95 @@
 #' @seealso <https://github.com/cwendt94/espn-api/blob/master/espn_api/football/constant.py>
 .espn_stat_map <- function() {
   c(
-    "3" = "passingYards",
-    "4" = "passingTouchdowns",
-    "8" = "passing25Yards",
-    "19" = "passing2PtConversions",
-    "20" = "passingInterceptions",
-    "24" = "rushingYards",
-    "25" = "rushingTouchdowns",
-    "26" = "rushing2PtConversions",
-    "28" = "rushing10Yards",
-    "35" = "rushing40YardTD",
-    "37" = "rushing100YardGame",
-    "38" = "rushing200YardGame",
-    "42" = "receivingYards",
-    "43" = "receivingTouchdowns",
-    "44" = "receiving2PtConversions",
-    "45" = "receiving40YardTD",
-    "48" = "receiving10Yards",
-    "53" = "receivingReceptions",
-    "56" = "receiving100YardGame",
-    "57" = "receiving200YardGame",
-    "63" = "fumbleRecoveryTouchdown",
-    "72" = "lostFumbles",
-    "74" = "madeFieldGoalsFrom50Plus",
-    "77" = "madeFieldGoalsFrom40To49",
-    "79" = "missedFieldGoalsFrom40To49",
-    "80" = "madeFieldGoalsFromUnder40",
-    "82" = "missedFieldGoalsFromUnder40",
-    "85" = "missedFieldGoals",
-    "86" = "madeExtraPoints",
-    "88" = "missedExtraPoints",
-    "89" = "defensive0PointsAllowed",
-    "90" = "defensive1To6PointsAllowed",
-    "91" = "defensive7To13PointsAllowed",
-    "92" = "defensive14To17PointsAllowed",
-    "93" = "defensiveBlockedKickForTouchdowns",
-    "95" = "defensiveInterceptions",
-    "96" = "defensiveFumbles",
-    "97" = "defensiveBlockedKicks",
-    "98" = "defensiveSafeties",
-    "99" = "defensiveSacks",
-    "101" = "kickoffReturnTouchdown",
-    "102" = "puntReturnTouchdown",
-    "103" = "fumbleReturnTouchdown",
-    "104" = "interceptionReturnTouchdown",
-    "114" = "kickoffReturnYards",
-    "115" = "puntReturnYards",
-    "122" = "defensive22To27PointsAllowed",
-    "123" = "defensive28To34PointsAllowed",
-    "124" = "defensive35To45PointsAllowed",
-    "125" = "defensive46+PointsAllowed",
-    "128" = "defensive000To099YardsAllowed",
-    "129" = "defensive100To199YardsAllowed",
-    "130" = "defensive200To299YardsAllowed",
-    "132" = "defensive350To399YardsAllowed",
-    "133" = "defensive400To449YardsAllowed",
-    "134" = "defensive450To499YardsAllowed",
-    "135" = "defensive500To549YardsAllowed",
-    "136" = "defensiveOver550YardsAllowed",
-
-    # Punter Stats
-    "140" = "puntsInsideThe10", # PT10
-    "141" = "puntsInsideThe20", # PT20
-    "148" = "puntAverage44.0+", # PTA44
-    "149" = "puntAverage42.0-43.9", # PTA42
-    "150" = "puntAverage40.0-41.9", # PTA40
-
-    # Head Coach stats
-    "161" = "25+pointsWinMargin", # WM25
-    "162" = "20-24pointWinMargin", # WM20
-    "163" = "15-19pointWinMargin", # WM15
-    "164" = "10-14pointWinMargin", # WM10
-    "165" = "5-9pointWinMargin", # WM5
-    "166" = "1-4pointWinMargin", # WM1
-
-    "155" = "TeamWin", # TW
-
-    "171" = "20-24pointLossMargin", # LM20
-    "172" = "25+pointLossMargin", # LM25
-
-    "198" = "madeFieldGoalsFrom50To59",
-    "200" = "missedFieldGoalsFrom50To59",
-    "201" = "madeFieldGoalsFrom60Plus",
-    "203" = "missedFieldGoalsFrom60Plus",
-    "206" = "2PtConversionReturnedForTouchdown",
-    "209" = "1PtSafety"
+    '0' = 'AB',
+    '1' = 'H',
+    '2' = 'AVG',
+    '3' = '2B',
+    '4' = '3B',
+    '5' = 'HR',
+    '6' = 'XBH', # 2B + 3B + HR
+    '7' = '1B',
+    '8' = 'TB', # 1 * COUNT(1B) + 2 * COUNT(2B) + 3 * COUNT(3B) + 4 * COUNT(HR)
+    '9' = 'SLG',
+    '10' = 'B_BB',
+    '11' = 'B_IBB',
+    '12' = 'HBP',
+    '13' = 'SF', # Sacrifice Fly
+    '14' = 'SH', # Sacrifice Hit - i.e. Sacrifice Bunt
+    '15' = 'SAC', # total sacrifices = SF + SH
+    '16' = 'PA',
+    '17' = 'OBP',
+    '18' = 'OPS', # OBP + SLG
+    '19' = 'RC', # Runs Created = TB * (H + BB) / (AB + BB)
+    '20' = 'R',
+    '21' = 'RBI',
+    # 22' = '',
+    '23' = 'SB',
+    '24' = 'CS',
+    '25' = 'SB-CS', # net steals
+    '26' = 'GDP',
+    '27' = 'B_SO', # batter strike-outs
+    '28' = 'PS', # pitches seen
+    '29' = 'PPA', # pitches per plate appearance = PS / PA
+    # 30' = '',
+    '31' = 'CYC',
+    '32' = 'GP', # pitcher games pitched
+    '33' = 'GS', # games started
+    '34' = 'OUTS',  # divide by 3 for IP
+    '35' = 'TBF',
+    '36' = 'P',  # pitches
+    '37' = 'P_H',
+    '38' = 'OBA', # Opponent Batting Average
+    '39' = 'P_BB',
+    '40' = 'P_IBB', # intentional walks allowed
+    '41' = 'WHIP',
+    '42' = 'HBP',
+    '43' = 'OOBP', # Opponent On-Base Percentage
+    '44' = 'P_R',
+    '45' = 'ER',
+    '46' = 'P_HR',
+    '47' = 'ERA',
+    '48' = 'K',
+    '49' = 'K/9',
+    '50' = 'WP',
+    '51' = 'BLK',
+    '52' = 'PK', # pickoff
+    '53' = 'W',
+    '54' = 'L',
+    '55' = 'WPCT', # Win Percentage
+    '56' = 'SVO', # Save opportunity
+    '57' = 'SV',
+    '58' = 'BLSV', # BLown SaVe
+    '59' = 'SV%', # Save percentage
+    '60' = 'HLD',
+    # 61' = '',
+    '62' = 'CG',
+    '63' = 'QS', # Quality Starts
+    # 64' = '',
+    '65' = 'NH', # No-hitters
+    '66' = 'PG', # Perfect Games
+    '67' = 'TC', # Total Chances = PO + A + E
+    '68' = 'PO', # Put Outs
+    '69' = 'A', # Assists
+    '70' = 'OFA', # Outfield Assists
+    '71' = 'FPCT', # Fielding Percentage
+    '72' = 'E',
+    '73' = 'DP', # Double plays turned
+    # Not sure what to call the next four
+    # 74 is games played where the batter's team won
+    # 75 is the same except when the team lost
+    # 76 and 77 are the same except for pitchers
+    '74' = 'B_G_W',
+    '75' = 'B_G_L',
+    '76' = 'P_G_W',
+    '77' = 'P_G_L',
+    # 78' = ,
+    # 79' = ,
+    # 80' = ,
+    '81' = 'G', # Games Played
+    '82' = 'K/BB', # Strikeout to Walk Ratio
+    '83' = 'SVHD', # Saves + Holds
+    '99' = 'STARTER'
   )
 }
 
